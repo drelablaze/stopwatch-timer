c5c3fce330909327acd185e5833c6c210e811cc3
drelablaze <dareosenirepo@gmail.com>
2024-03-05 12:17:18 +0300

    changes-done

--- round-timer.js
+++ round-timer.js
@@ -5,13 +5,19 @@ const timeDisplay = document.getElementById("timer-container-js");
 const startBTN = document.getElementById("start-js");
 const pauseBTN = document.getElementById("pause-js");
 const stopBTN = document.getElementById("stop-js");
+const displayEl = document.getElementById("display-js");
 
 let [seconds, minutes, hours] = [0, 0, 0];
 let hour = "";
 let second = "";
 let minute = "";
+let timerRunning = false;
+let timer = null;
 
 function startTimer() {
+  if (timer !== null) {
+    clearInterval(timer);
+  }
   function roundTimer() {
     seconds++;
     if (seconds === 60) {
@@ -30,9 +36,30 @@ function startTimer() {
 
     timeDisplay.innerHTML = hour + ":" + minute + ":" + second;
     timeDisplay.classList.add("timer-add");
-    timeDisplay.classList.remove('.timer-container"');
+
+    let displayValue =
+      timerRunning === !true ? "Timer started" : "Timer paused";
+    displayEl.textContent = `${displayValue}`;
   }
-  setInterval(roundTimer, 1000);
+  timer = setInterval(roundTimer, 1000);
 }
 
 startBTN.addEventListener("click", startTimer);
+
+function pauseTimer() {
+  clearInterval(timer);
+  let displayValue = timerRunning === true ? "Timer started" : "Timer paused";
+  displayEl.textContent = `${displayValue}`;
+}
+
+pauseBTN.addEventListener("click", pauseTimer);
+
+function stopTimer() {
+  clearInterval(timer);
+  [seconds, minutes, hours] = [0, 0, 0];
+  timeDisplay.innerHTML = "00" + ":" + "00" + ":" + "00";
+  timeDisplay.classList.add("timer-container");
+  displayEl.textContent = `Timer stopped`;
+}
+
+stopBTN.addEventListener("click", stopTimer);
